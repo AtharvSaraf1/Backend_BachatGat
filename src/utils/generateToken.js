620c0574050252797338245656332f427590f9fe
AtharvSaraf1 <atharvsaraf8@gmail.com>
2026-05-10 07:41:06 +0530

    resolved issue of generate token

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,9 +1,11 @@
 const jwt = require("jsonwebtoken");
 
-function generateToken(userId) {
-    return jwt.sign({ id: userId },
-        process.env.JWT_SECRET, { expiresIn: "7d" }
+const generateToken = (id, roleSelection) => {
+    return jwt.sign(
+        { id, roleSelection },
+        process.env.JWT_SECRET,
+        { expiresIn: "7d" }
     );
-}
+};
 
 module.exports = generateToken;
